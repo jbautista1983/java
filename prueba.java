15e0492c01c1a2b96d62ceb5a2f535b012589d77
jbautista1983 <jbautista1983@hotmail.com>
2017-01-02 12:59:48 -0300

    Add files via upload

---
+++ prueba.java
@@ -0,0 +1,33 @@
+package prueba_mongo_db;
+
+import com.mongodb.DB;
+import com.mongodb.MongoClient;
+
+public class prueba {
+	
+
+
+	   public static void main( String  args[] ) {
+		
+	      try{
+			
+	         // To connect to mongodb server
+	         MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
+				
+	         // Now connect to your databases
+	         DB db = mongoClient.getDB( "test" );
+	         System.out.println("Connect to database successfully");
+	         String myUserName = "usuario";
+	         String myPassword = "contraseña";
+//         boolean auth = db.authenticate(myUserName, myPassword);
+       		boolean auth = db.authenticate("username", "password".toCharArray());
+	         System.out.println("Authentication: "+auth);
+				
+	      }catch(Exception e){
+	         System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+	      }
+ 	   }
+	}
+
+	
+}
